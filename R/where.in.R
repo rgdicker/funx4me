df46721e9d4cdf752f0ffc500dfa97416533edd3
JamesBlonde <ryandickerson@ryans-MacBook-Pro-6.local>
2016-11-19 16:57:02 -0500

    fix wherein order

--- R/where.in.R
+++ R/where.in.R
@@ -1,19 +1,19 @@
 #' @export
-where.in <- function(smallvec,bigvec,fullvec=F){
+where.in <- function(bigvec,smallvec,fullvec=F){
     # Where in bigvec is smallvec?
     # So e.g. where.in(bigvec[10:12],bigvec) == 10:12
     N <- length(bigvec)
     n <- length(smallvec)
     b <- 1:n
-    if(is.integer(bigvec) & !is.integer(smallvec)) 
+    if(is.integer(bigvec) & !is.integer(smallvec))
         bigvec <- as.numeric(bigvec)
-    if(!is.integer(bigvec) & is.integer(smallvec)) 
+    if(!is.integer(bigvec) & is.integer(smallvec))
         smallvec <- as.numeric(smallvec)
     for(i in 0:(N-n)){
         if(identical(bigvec[b+i],smallvec)){
             if(fullvec) return((i+1):(i+n))
             else return(i+1)
-        } 
+        }
     }
     return(FALSE)
 }
